a7b14806bab89241708c0ab9f8af09924060c3fd
Aasifali789 <muhammadaasifjagirani789@gmail.com>
2022-05-22 00:46:45 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// import 'package:Mall_App/splash.dart';
+import 'package:flutter/material.dart';
+
+import 'splash.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.lightBlue),
+        title: 'Wernhil',
+        home: Splash());
+  }
+}
